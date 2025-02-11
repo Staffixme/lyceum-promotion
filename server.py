ea9602fa54793a07b723f305add3ebf73f1a4112
Sys10 <none@none.none>
2025-02-11 12:03:28 +0300

    promotion with image commit

--- server.py
+++ server.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def start_page():
     return "<h1>Миссия Колонизация Марса!</h1>"
 
+
 @app.route('/index')
 def index():
     return "И на Марсе будут яблони цвести!"
+
 
 @app.route('/promotion')
 def promotion():
@@ -32,6 +34,44 @@ def image_mars():
                         <img src="{url_for('static', filename='img/mars.jpg')}" 
                         alt="здесь должна была быть картинка, но не нашлась">
                         <br>Вот она какая, красная планета.
+                      </body>
+                    </html>"""
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static',
+                                                                              filename='css/style.css')}" />
+                         <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars.jpg')}" 
+                        alt="здесь должна была быть картинка, но не нашлась">
+                        <br>
+                        <div class="alert alert-dark" role="alert">
+                        Человечество вырастает из детства.
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                        Человечеству мала одна планета.
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                        Мы сделаем обитаемыми безжизненные пока планеты.
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                        И начнем с Марса!
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                        Присоединяйся!
+                        </div>
                       </body>
                     </html>"""
 
