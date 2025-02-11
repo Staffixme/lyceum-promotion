2aa064bbf6c0dcd9aa3255b63f8f5452f2753136
Sys10 <none@none.none>
2025-02-11 11:53:44 +0300

    mars image commit

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -17,6 +17,23 @@ def promotion():
                       "Мы сделаем обитаемыми безжизненные пока планеты.", "И начнем с Марса!",
                       "Присоединяйся!")
     return "<br>".join(promotion_list)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars.jpg')}" 
+                        alt="здесь должна была быть картинка, но не нашлась">
+                        <br>Вот она какая, красная планета.
+                      </body>
+                    </html>"""
 
 
 if __name__ == '__main__':
